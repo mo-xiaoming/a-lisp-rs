9f8245e5e6cb0cf0c48c1fa901bfe0d226e91fae
Mo Xiaoming <2188767+mo-xiaoming@users.noreply.github.com>
2023-03-09 23:15:10 +0800

    refactor tests

--- src/combinators.rs
+++ src/combinators.rs
@@ -1,126 +1,184 @@
 #![allow(unused)]
 
-#[derive(Debug)]
-pub(crate) struct SourceRange {
-    begin: usize,
-    end: usize,
+use crate::lexer::Cursor;
+
+use crate::lexer::{Source, SourceRange, Token};
+
+pub(crate) trait ParseError<'src> {
+    fn description(&'src self, src: &'src Source) -> String;
 }
 
-#[derive(Debug)]
-pub(crate) enum ParseError<'src> {
-    Unexpected {
-        src: &'src Source,
-        expected: String,
-        got: SourceRange,
-    },
-    EarlyEof {
-        src: &'src Source,
-        expected: String,
-    },
+type IResult<'src, O, E> = Result<(Cursor, O), E>;
+
+#[derive(Debug, Clone)]
+struct EarlyEof<'src> {
+    src: &'src Source,
+    expected: String,
 }
 
-impl<'src> std::fmt::Display for ParseError<'src> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            ParseError::Unexpected { src, expected, got } => write!(
-                f,
-                "expected `{}`, got `{}`",
-                expected,
-                &src.content[got.begin..=got.end]
-            ),
-            ParseError::EarlyEof { src, expected } => {
-                write!(f, "expected `{}`, but reached the end of file", expected)
-            }
-        }
+impl<'src> ParseError<'src> for EarlyEof<'src> {
+    fn description(&'src self, src: &'src Source) -> String {
+        todo!()
     }
 }
 
-impl<'src> std::error::Error for ParseError<'src> {}
-
-#[derive(Debug, Default, Clone, Copy, std::hash::Hash, PartialEq, Eq, PartialOrd, Ord)]
-pub(crate) struct Cursor {
-    r: usize,
-    c: usize,
-    off: usize,
+#[derive(Debug, Clone)]
+pub struct ParseCharError<'src> {
+    expected: String,
+    got: SourceRange<'src>,
 }
 
-type IResult<'src, O> = Result<(Cursor, O), ParseError<'src>>;
-
-#[derive(Debug)]
-pub(crate) struct Source {
-    content: String,
+impl<'src> ParseError<'src> for ParseCharError<'src> {
+    fn description(&'src self, src: &'src Source) -> String {
+        todo!()
+    }
 }
 
-fn char(c: char) -> impl Fn(&Source, Cursor) -> IResult<'_, char> {
+fn char(c: char) -> impl Fn(&Source, Cursor) -> IResult<'_, (char, SourceRange), ParseCharError> {
     move |src: &Source, mut cursor: Cursor| {
-        let x = src
-            .content
-            .chars()
-            .next()
-            .ok_or_else(|| ParseError::EarlyEof {
-                src,
-                expected: c.to_string(),
-            })?;
+        assert!(!src.is_empty(), "char parser doesn't support empty source");
+        assert!(
+            !src.is_empty() && src.len() > cursor.0,
+            "char parser reaches EOF before parsing"
+        );
+
+        let x = src.char().unwrap();
 
         if x == c {
-            cursor.c += 1;
-            cursor.off += 1;
-            Ok((cursor, x))
+            cursor.0 += 1;
+            Ok((
+                cursor,
+                (
+                    x,
+                    SourceRange {
+                        src,
+                        begin: cursor.0 - 1,
+                        end: cursor.0,
+                    },
+                ),
+            ))
         } else {
-            Err(ParseError::Unexpected {
-                src,
+            Err(ParseCharError {
                 expected: c.to_string(),
                 got: SourceRange {
-                    begin: cursor.off,
-                    end: cursor.off + 1,
+                    src,
+                    begin: cursor.0,
+                    end: cursor.0 + 1,
                 },
             })
         }
     }
 }
 
-pub(crate) fn lparen() -> impl Fn(&Source, Cursor) -> IResult<'_, char> {
-    move |src: &Source, mut cursor: Cursor| char('(')(src, cursor)
-}
-
-pub(crate) fn rparen() -> impl Fn(&Source, Cursor) -> IResult<'_, char> {
-    move |src: &Source, mut cursor: Cursor| char(')')(src, cursor)
-}
-
-pub(crate) fn integer() -> impl Fn(&Source, Cursor) -> IResult<'_, i64> {
+pub fn lparen() -> impl Fn(&Source, Cursor) -> IResult<'_, Token, ParseCharError> {
     move |src: &Source, mut cursor: Cursor| {
-        let ef = || String::from("{integer}");
+        char('(')(src, cursor).map(|(i, (_, loc))| (i, Token::LParen { loc }))
+    }
+}
 
-        if src.content.is_empty() {
-            return Err(ParseError::EarlyEof {
-                src,
-                expected: ef(),
-            });
-        }
+pub fn rparen() -> impl Fn(&Source, Cursor) -> IResult<'_, Token, ParseCharError> {
+    move |src: &Source, mut cursor: Cursor| {
+        char(')')(src, cursor).map(|(i, (_, loc))| (i, Token::RParen { loc }))
+    }
+}
 
-        let s = src
-            .content
-            .chars()
-            .take_while(|c| c.is_numeric())
-            .collect::<String>();
-        if s.is_empty() {
-            return Err(ParseError::Unexpected {
-                src,
-                expected: ef(),
+#[derive(Debug, Clone)]
+pub enum ParseIntegerError<'src> {
+    Unexpected { got: SourceRange<'src> },
+    Overflow { got: SourceRange<'src> },
+    LeadingZero { got: SourceRange<'src> },
+}
+
+pub fn integer() -> impl Fn(&Source, Cursor) -> IResult<'_, Token, ParseIntegerError> {
+    move |src: &Source, mut cursor: Cursor| {
+        assert!(
+            !src.is_empty(),
+            "integer parser doesn't support empty source"
+        );
+
+        if src.char() == Some('0') {
+            return Err(ParseIntegerError::LeadingZero {
                 got: SourceRange {
-                    begin: cursor.off,
-                    end: cursor.off + 1,
+                    src,
+                    begin: cursor.0,
+                    end: cursor.0 + 1,
                 },
             });
         }
 
-        Ok((
-            Cursor {
-                r: cursor.r,
-                c: cursor.c + s.len(),
-                off: cursor.off + s.len(),
+        let s = src.take_while(|c| c.is_ascii_digit()).ok_or({
+            ParseIntegerError::Unexpected {
+                got: SourceRange {
+                    src,
+                    begin: cursor.0,
+                    end: cursor.0 + 1,
+                },
+            }
+        })?;
+
+        let i = s.parse::<u64>().map_err(|e| match e.kind() {
+            std::num::IntErrorKind::PosOverflow => ParseIntegerError::Overflow {
+                got: SourceRange {
+                    src,
+                    begin: cursor.0,
+                    end: cursor.0 + s.len(),
+                },
             },
-            s.parse::<i64>().unwrap(),
+            _ => unreachable!("unexpected {{integer}} parsing error"),
+        })?;
+
+        Ok((
+            Cursor(cursor.0 + s.len()),
+            Token::Integer {
+                v: i,
+                loc: SourceRange {
+                    src,
+                    begin: cursor.0,
+                    end: cursor.0 + s.len(),
+                },
+            },
+        ))
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct ParseIdentifierError<'src> {
+    got: SourceRange<'src>,
+}
+
+pub fn identifier() -> impl Fn(&Source, Cursor) -> IResult<'_, Token, ParseIdentifierError> {
+    move |src: &Source, mut cursor: Cursor| {
+        assert!(
+            !src.is_empty(),
+            "identifier parser doesn't support empty source"
+        );
+
+        let ef = || ParseIdentifierError {
+            got: SourceRange {
+                src,
+                begin: cursor.0,
+                end: cursor.0 + 1,
+            },
+        };
+
+        let s = src
+            .take_while(|c| c.is_ascii_alphanumeric() || c == '_')
+            .ok_or_else(ef)?;
+        s.chars()
+            .next()
+            .filter(|c| !c.is_ascii_digit())
+            .ok_or_else(ef)?;
+
+        Ok((
+            Cursor(cursor.0 + s.len()),
+            Token::Identifier {
+                v: s.to_owned(),
+                loc: SourceRange {
+                    src,
+                    begin: cursor.0,
+                    end: cursor.0 + s.len(),
+                },
+            },
         ))
     }
 }
@@ -129,42 +187,218 @@ pub(crate) fn integer() -> impl Fn(&Source, Cursor) -> IResult<'_, i64> {
 mod test {
     use super::*;
 
-    fn char_tests<P>(c: char, p: P)
-    where
-        P: Fn(&Source, Cursor) -> IResult<'_, char>,
-    {
-        let source = Source {
-            content: c.to_string(),
-        };
-        let r = p(&source, Cursor::default())
-            .unwrap_or_else(|_| panic!("`{}` should be properly accepted", c));
-        assert_eq!(r, (Cursor { r: 0, c: 1, off: 1 }, c));
-
-        let source = Source {
-            content: String::new(),
-        };
-        let r = p(&source, Cursor::default());
-        assert!(
-            matches!(r, Err(ParseError::EarlyEof { expected,.. }) if expected == c.to_string())
-        );
-
-        let source = Source {
-            content: "x".to_owned(),
-        };
-        let r = p(&source, Cursor::default());
-        assert!(
-            matches!(r, Err(ParseError::Unexpected { expected, got: SourceRange { begin, end }, .. }) if expected == c.to_string() && begin == 0 && end == 1)
-        );
+    #[test]
+    #[should_panic(expected = "char parser doesn't support empty source")]
+    fn lparen_panic_on_empty_source1() {
+        let source = Source::new(String::new(), "hi");
+        let _r = lparen()(&source, Cursor::default());
     }
 
     #[test]
-    fn test_char() {
-        char_tests('c', char('c'));
+    #[should_panic(expected = "char parser reaches EOF before parsing")]
+    fn lparen_panic_on_empty_source2() {
+        let content = "hi".to_owned();
+        let source = Source::new(content, "hi");
+        let _r = lparen()(&source, Cursor(source.len()));
+    }
+
+    #[test]
+    #[should_panic(expected = "char parser doesn't support empty source")]
+    fn rparen_panic_on_empty_source_1() {
+        let source = Source::new(String::new(), "hi");
+        let _r = rparen()(&source, Cursor::default());
+    }
+
+    #[test]
+    #[should_panic(expected = "char parser reaches EOF before parsing")]
+    fn rparen_panic_on_empty_source2() {
+        let content = "hi".to_owned();
+        let source = Source::new(content, "hi");
+        let _r = rparen()(&source, Cursor(source.len()));
+    }
+
+    fn test_paren_ok<P, T>(parser: &P, c: char, content: &str, t: &T)
+    where
+        P: Fn(&Source, Cursor) -> IResult<'_, Token, ParseCharError>,
+        T: for<'src> Fn(&'src Source) -> Token<'src>,
+    {
+        let source = Source::new(content.to_owned(), "hi");
+        let r = parser(&source, Cursor::default());
+        assert!(matches!(
+            r,
+            Ok((
+                Cursor(1),
+                token,
+            )) if token == t(&source)
+        ));
+    }
+
+    fn test_paren_err<P>(parser: &P, c: char)
+    where
+        P: Fn(&Source, Cursor) -> IResult<'_, Token, ParseCharError>,
+    {
+        let source = Source::new("x".to_owned(), "hi");
+        let r = parser(&source, Cursor::default());
+        assert!(
+            matches!(r, Err(ParseCharError{ expected, got: SourceRange {  begin: 0, end: 1,.. } }) if expected == c.to_string() )
+        );
     }
 
     #[test]
     fn test_parens() {
-        char_tests('(', lparen());
-        char_tests(')', rparen());
+        let p = lparen();
+        fn lp(src: &Source) -> Token {
+            Token::LParen {
+                loc: SourceRange {
+                    src,
+                    begin: 0,
+                    end: 1,
+                },
+            }
+        };
+
+        test_paren_ok(&p, '(', "(", &lp);
+        test_paren_ok(&p, '(', "(xx", &lp);
+        test_paren_err(&p, '(');
+
+        let p = rparen();
+        fn rp(src: &Source) -> Token {
+            Token::RParen {
+                loc: SourceRange {
+                    src,
+                    begin: 0,
+                    end: 1,
+                },
+            }
+        };
+
+        test_paren_ok(&p, ')', ")", &rp);
+        test_paren_ok(&p, ')', ")xx", &rp);
+        test_paren_err(&p, ')');
+    }
+
+    #[test]
+    #[should_panic(expected = "integer parser doesn't support empty source")]
+    fn integer_panics_on_empty_source() {
+        let source = Source::new(String::new(), "hi");
+        let _r = integer()(&source, Cursor::default());
+    }
+
+    #[test]
+    fn test_integer() {
+        let sf = |(s, len): (&str, usize)| {
+            let source = Source::new(s.to_owned(), "hi");
+            let r = integer()(&source, Cursor::default());
+            assert!(matches!(
+                r,
+                Ok((
+                    Cursor(off),
+                    Token::Integer {
+                        v: 12345,
+                        loc: SourceRange {
+                            begin: 0,
+                            end,
+                            ..
+                        }
+                    }
+                )) if off == 5 && end == 5
+            ));
+        };
+
+        for s in [("12345", 5), ("12345abc", 5)] {
+            sf(s);
+        }
+
+        let source = Source::new("012345".to_owned(), "hi");
+        let r = integer()(&source, Cursor::default());
+        assert!(matches!(
+            r,
+            Err(ParseIntegerError::LeadingZero {
+                got: SourceRange {
+                    begin: 0,
+                    end: 1,
+                    ..
+                }
+            })
+        ));
+
+        let source = Source::new("abc".to_owned(), "hi");
+        let r = integer()(&source, Cursor::default());
+        assert!(matches!(
+            r,
+            Err(ParseIntegerError::Unexpected {
+                got: SourceRange {
+                    begin: 0,
+                    end: 1,
+                    ..
+                }
+            })
+        ));
+
+        let ov = "18446744073709551616".to_owned(); // u64::max + 1
+        let source = Source::new(ov.clone(), "hi");
+        let r = integer()(&source, Cursor::default());
+        assert!(matches!(
+            r,
+            Err(ParseIntegerError::Overflow {
+                got: SourceRange {
+                    begin: 0,
+                    end,
+                    ..
+                }
+            }) if end == ov.len()
+        ));
+    }
+
+    #[test]
+    #[should_panic(expected = "identifier parser doesn't support empty source")]
+    fn identifier_panics_on_empty_source() {
+        let source = Source::new(String::new(), "hi");
+        let _r = identifier()(&source, Cursor::default());
+    }
+
+    #[test]
+    fn test_identifier() {
+        let sf = |s: &str| {
+            let source = Source::new(s.to_owned(), "hi");
+            let r = identifier()(&source, Cursor::default());
+            assert!(matches!(
+                r,
+                Ok((
+                    Cursor(off),
+                    Token::Identifier {
+                        v,
+                        loc: SourceRange {
+                            begin: 0,
+                            end,
+                            ..
+                        }
+                    }
+                )) if off == s.len() && end == s.len() && v == s
+            ));
+        };
+
+        for s in ["abc", "_ab", "_3a", "a_b_c", "a_3", "a3_", "__", "_", "__a"] {
+            sf(s);
+        }
+
+        let ef = |s: &str| {
+            let source = Source::new(s.to_owned(), "hi");
+            let r = identifier()(&source, Cursor::default());
+            assert!(matches!(
+                r,
+                Err(ParseIdentifierError {
+                    got: SourceRange {
+                        begin: 0,
+                        end: 1,
+                        ..
+                    }
+                })
+            ));
+        };
+
+        for s in ["3ab", "3_ab", "#ab", "@ab"] {
+            ef(s);
+        }
     }
 }
--- src/lexer.rs
+++ src/lexer.rs
@@ -1,24 +1,101 @@
 #![allow(unused)]
 
-#[derive(Debug)]
-pub enum Token {
-    Integer(i64), // signed 64-bit integer
-    Float(f64),
-    String(String),
-    Symbol(String), // any group of characters other than an integer or parenthesis or whitespace
-    LParen,
-    RParen,
+#[derive(Debug, Clone, Eq)]
+pub struct Source {
+    content: String,
+    path: std::path::PathBuf,
 }
 
-impl std::fmt::Display for Token {
+impl PartialEq for Source {
+    fn eq(&self, other: &Self) -> bool {
+        self.path == other.path
+    }
+}
+
+impl Source {
+    pub(crate) fn new<P: AsRef<std::path::Path>>(content: String, path: P) -> Self {
+        Self {
+            content,
+            path: path.as_ref().to_owned(),
+        }
+    }
+}
+
+impl std::fmt::Display for Source {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "path: {}", self.path.display())
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy, std::hash::Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Cursor(pub usize);
+
+impl Source {
+    pub fn is_empty(&self) -> bool {
+        self.content.is_empty()
+    }
+    pub fn len(&self) -> usize {
+        self.content.len()
+    }
+    pub fn take_while<P: Fn(char) -> bool>(&self, p: P) -> Option<&str> {
+        if (self.is_empty()) {
+            return None;
+        }
+
+        match self.content.find(|c| !p(c)) {
+            Some(0) => None,
+            Some(n) => Some(&self.content[..n]),
+            None => Some(&self.content),
+        }
+    }
+    pub fn take_n(&self, n: usize) -> Option<&str> {
+        if self.content.len() < n || n == 0 {
+            None
+        } else {
+            Some(&self.content[..n])
+        }
+    }
+    pub fn char(&self) -> Option<char> {
+        self.content.chars().next()
+    }
+}
+
+fn is_eof(src: &Source, cursor: Cursor) -> bool {
+    src.is_empty() || src.len() <= cursor.0
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct SourceRange<'src> {
+    pub(crate) src: &'src Source,
+    pub(crate) begin: usize,
+    pub(crate) end: usize,
+}
+
+impl<'src> std::fmt::Display for SourceRange<'src> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}:{}-{}", self.src, self.begin, self.end - 1)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Token<'src> {
+    Integer { v: u64, loc: SourceRange<'src> }, // signed 64-bit integer
+    Float { v: f64, loc: SourceRange<'src> },
+    String { v: String, loc: SourceRange<'src> },
+    Identifier { v: String, loc: SourceRange<'src> },
+    LParen { loc: SourceRange<'src> },
+    RParen { loc: SourceRange<'src> },
+}
+
+impl<'src> std::fmt::Display for Token<'src> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Token::Integer(n) => write!(f, "{}", n),
-            Token::Float(n) => write!(f, "{}", n),
-            Token::String(s) => write!(f, "{}", s),
-            Token::Symbol(s) => write!(f, "{}", s),
-            Token::LParen => write!(f, "("),
-            Token::RParen => write!(f, ")"),
+            Token::Integer { v, loc } => write!(f, "{}: `{}::Integer`", loc, v),
+            Token::Float { v, loc } => write!(f, "{}: `{}::Float`", loc, v),
+            Token::String { v, loc } => write!(f, "{}: `{}::String`", loc, v),
+            Token::Identifier { v, loc } => write!(f, "{}: `{}::Identifier`", loc, v),
+            Token::LParen { loc } => write!(f, "{}: `LParen`", loc),
+            Token::RParen { loc } => write!(f, "{}: `RParen`", loc),
         }
     }
 }
@@ -29,14 +106,80 @@ mod test {
 
     #[test]
     fn fmt_token() {
-        assert_eq!(format!("{}", Token::Integer(42)), "42");
-        assert_eq!(format!("{}", Token::Float(42.42)), "42.42");
+        let src = Source::new(String::new(), "hi");
+        let loc = SourceRange {
+            src: &src,
+            begin: 0,
+            end: 3,
+        };
         assert_eq!(
-            format!("{}", Token::String("hello world".to_owned())),
-            "hello world"
+            format!("{}", Token::Integer { v: 42, loc }),
+            format!("{}: `42::Integer`", loc)
         );
-        assert_eq!(format!("{}", Token::Symbol("foo".to_owned())), "foo");
-        assert_eq!(format!("{}", Token::LParen), "(");
-        assert_eq!(format!("{}", Token::RParen), ")");
+        assert_eq!(
+            format!("{}", Token::Float { v: 42.42, loc }),
+            format!("{}: `42.42::Float`", loc)
+        );
+        assert_eq!(
+            format!(
+                "{}",
+                Token::String {
+                    v: "hello world".to_owned(),
+                    loc
+                }
+            ),
+            format!("{}: `hello world::String`", loc)
+        );
+        assert_eq!(
+            format!(
+                "{}",
+                Token::Identifier {
+                    v: "foo".to_owned(),
+                    loc
+                }
+            ),
+            format!("{}: `foo::Identifier`", loc)
+        );
+        assert_eq!(
+            format!("{}", Token::LParen { loc }),
+            format!("{}: `LParen`", loc)
+        );
+        assert_eq!(
+            format!("{}", Token::RParen { loc }),
+            format!("{}: `RParen`", loc)
+        );
+    }
+
+    #[test]
+    fn test_source() {
+        let source = Source::new(String::new(), "hi");
+
+        assert!(source.is_empty());
+        assert!(is_eof(&source, Cursor(0)));
+        assert_eq!(source.len(), 0);
+        assert_eq!(source.take_while(|c| c == ' '), None);
+        assert_eq!(source.take_n(0), None);
+        assert_eq!(source.take_n(1), None);
+        assert_eq!(source.char(), None);
+
+        let content = "aabc";
+        let source = Source::new(content.to_owned(), "hi");
+
+        assert!(!source.is_empty());
+        for i in 0..6 {
+            if i < source.len() {
+                assert!(!is_eof(&source, Cursor(i)));
+            } else {
+                assert!(is_eof(&source, Cursor(i)));
+            }
+        }
+        assert_eq!(source.len(), 4);
+        assert_eq!(source.take_while(|c| c == 'a'), Some("aa"));
+        assert_eq!(source.take_while(|c| c == 'b'), None);
+        assert_eq!(source.take_n(0), None);
+        assert_eq!(source.take_n(1), Some("a"));
+        assert_eq!(source.take_n(source.len()), Some(content));
+        assert_eq!(source.take_n(source.len() + 1), None);
+        assert_eq!(source.char(), Some('a'));
     }
 }
